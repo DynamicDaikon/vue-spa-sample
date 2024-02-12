9ba4adbe2cf53a72434e4d07ab1052352693a82b
DynamicDaikon <hietasoba@gmail.com>
2024-02-12 14:06:35 +0900

    add input component

---
+++ src/views/forInput.vue
@@ -0,0 +1,16 @@
+<template>
+  <input v-model="text" placeholder="Type here" />
+  <p>{{ text }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: '',
+    };
+  },
+};
+</script>
+
+<style></style>
